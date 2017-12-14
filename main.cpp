3be9012cd7a7b39f21d9cb150f5a90fb776c9eb4
Goh Zi He <zihe.goh@gmail.com>
2017-12-14 18:00:56 +0800

    Added VSCode project

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
+/*
+Create a C++ console application to retrieve items in specific Gmail mailbox 
+(filtered by specified period of time) and store them in the same folder in encrypted form 
+(algorithm should be chosen by yourself). 
+
+Parameters are: mailbox name, mailbox password, start of the retrieval period, end of the retrieval period. 
+You need to use official API provided by Google, no IMAP/POP3. 
+*/
+
 #include <iostream>
+#include <string>
+
+bool RetrieveMail(const std::string &mailBox, const std::string &pass, int start, int end);
 
 int main()
 {
+    std::cout << "aaa";
     return 0;
 }
