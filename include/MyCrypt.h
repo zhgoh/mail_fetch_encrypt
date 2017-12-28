38ae6674159a0bee3ce96b26b6cd10fb022937e1
Zi He <zihe.goh@gmail.com>
2017-12-29 00:13:54 +0800

    Added Envelope open/close WIP

--- include/MyCrypt.h
+++ include/MyCrypt.h
@@ -7,6 +7,45 @@
 
 #include <string>
 
+class File
+{
+public:
+    File(const char *file, const char *mode);
+    ~File();
+    
+    bool IsOpen() const;
+    FILE *Get() const;
+
+private:
+    FILE *file;
+};
+
+class Buffer
+{
+public:
+    Buffer(size_t sz);
+    char *Get() const;
+    ~Buffer();
+    
+private:
+    char *ptr;
+};
+
+class CipherText
+{
+public:
+    CipherText(const char *str, int len);
+    ~CipherText();
+    char *str();
+    int &len();
+    size_t size() const;
+
+private:
+    char *ptr;
+    int length;
+};
+
+void EncryptInit();
 void EncryptFile(const char *plainMessage, const char *outputFile);
 void DecryptFile(const char *inputFile, std::string &plainMessage);
 bool Encrypt(const char *plainMessage, char *cipherText, int *cipherLen);
--- src/MyCrypt.cpp
+++ src/MyCrypt.cpp
@@ -3,99 +3,107 @@
 //
 
 #include "MyCrypt.h"
+
+#include <glog/logging.h>
 #include <openssl/evp.h>
+#include <openssl/pem.h>
 #include <cstring>
 #include <iostream>
 #include <fstream>
-#include <sstream>
+#include <algorithm>
 
-/* Please choose an encryption mode in the following defines value.
-   Only 1 encryption mode is allow at a time. Blowfish CBC mode is
-   the default encryption in this file.
- */
+bool Envelope_Seal(const unsigned char *plain_text, int plain_text_len,
+                   unsigned char **encrypted_key, int *encrypted_key_len,
+                   unsigned  char *iv,
+                   unsigned char *cipher_text, int *cipher_text_len);
 
-// BlowFish
-#define MY_CIPHER_MODE EVP_bf_cbc()     // Blowfish CBC mode
-//#define MY_CIPHER_MODE EVP_bf_ecb()   // Blowfish ECB mode
+bool Envelope_Open(unsigned char *cipher_text, int cipher_text_len,
+                   unsigned char *encrypted_key, int encrypted_key_len, unsigned char *iv,
+                   unsigned char *plain_text, int *plain_text_len);
 
-// DES
-//#define MY_CIPHER_MODE EVP_des_cbc()    // DES CBC mode
-//#define MY_CIPHER_MODE EVP_des_ecb()    // DES ECB mode
-//#define MY_CIPHER_MODE EVP_des_ede()    // DES EDE mode
-//#define MY_CIPHER_MODE EVP_des_ede3()   // DES EDE3 mode
+static EVP_PKEY *PUB_KEY = nullptr;
+static EVP_PKEY *PRI_KEY = nullptr;
+static unsigned char IV[] = "45213456a23fdg";
 
-// RC2
-//#define MY_CIPHER_MODE EVP_rc2_cbc()    // RC2 CBC mode
-//#define MY_CIPHER_MODE EVP_rc2_ecb()    // RC2 ECB mode
+static unsigned char encryptedKey[512];
+static unsigned char *eKeys = encryptedKey;
+static int encryptedKeyLen = 0;
 
-// RC4
-//#define MY_CIPHER_MODE EVP_rc4()      // RC4 mode
-//#define MY_CIPHER_MODE EVP_rc4_40()   // RC4 40 mode
-
-// TODO: Don't store keys here
-static const unsigned char key[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-static const unsigned char iv[] = {1,2,3,4,5,6,7,8};
-
-
-class CipherText
+void EncryptInit()
 {
-public:
-    CipherText(const char *str, size_t len) :
-            ptr(new char[len]), length(int(len))
+    File pub_keyFile("rsa_pubkey.pem", "rb");
+    if (pub_keyFile.IsOpen())
     {
-        memcpy(ptr, str, len);
+        PUB_KEY = PEM_read_PUBKEY(pub_keyFile.Get(), nullptr, nullptr, nullptr);
+        if (!PUB_KEY)
+        {
+            LOG(ERROR) << "Load Public Key Failed" << std::endl;
+        }
+        else
+        {
+            std::cout << "Loaded Public Key successfully" << std::endl;
+        }
     }
     
-    ~CipherText()
+    File pri_keyFile("rsa_privkey.pem", "rb");
+    if (pri_keyFile.IsOpen())
     {
-        delete[] ptr;
+        PRI_KEY = PEM_read_PrivateKey(pri_keyFile.Get(), nullptr, nullptr, nullptr);
+        if (!PRI_KEY)
+        {
+            LOG(ERROR) << "Load Private Key Failed" << std::endl;
+        }
+        else
+        {
+            std::cout << "Loaded Private Key successfully" << std::endl;
+        }
     }
-    
-    char *str()
-    {
-        return ptr;
-    }
-    
-    int &len()
-    {
-        return length;
-    }
-    
-private:
-    char *ptr;
-    int length;
-};
+}
 
 void EncryptFile(const char *plainMessage, const char *outputFile)
 {
-    CipherText encrypted(plainMessage, strlen(plainMessage));
-    if (Encrypt(plainMessage, encrypted.str(), &encrypted.len()))
+    auto len = static_cast<int>(strlen(plainMessage));
+    
+
+    CipherText encrypted(plainMessage, len);
+    if (Envelope_Seal(reinterpret_cast<const unsigned char *>(plainMessage), len,
+                      &eKeys, &encryptedKeyLen,
+                      IV,
+                      reinterpret_cast<unsigned char *>(encrypted.str()), &encrypted.len()))
     {
-        FILE *file = fopen(outputFile, "wb");
-        if (file)
+        File ec("ec", "wb");
+        if (ec.IsOpen())
         {
-            fwrite(encrypted.str(), sizeof(char), encrypted.len(), file);
+            //PEM_write(ec.Get(), "EncryptedKey", "", encryptedKey, encryptedKeyLen);
         }
-        fclose(file);
+        
+        File file(outputFile, "wb");
+        if (file.IsOpen())
+        {
+            fwrite(file.Get(), sizeof(char), encrypted.size(), file.Get());
+        }
     }
+    
 }
 
 void DecryptFile(const char *inputFile, std::string &plainMessage)
 {
-    FILE *file = fopen(inputFile, "rb");
-    if (file)
+    File file(inputFile, "rb");
+    if (file.IsOpen())
     {
         // obtain file size:
-        fseek (file , 0 , SEEK_END);
-        long lSize = ftell (file);
-        rewind (file);
+        fseek (file.Get() , 0 , SEEK_END);
+        long lSize = ftell (file.Get());
+        rewind (file.Get());
     
         // allocate memory to contain the whole file:
-        char *buffer = new char[lSize];
-        size_t result = fread (buffer, 1, size_t(lSize), file);
-        
-        CipherText decrypted(buffer, size_t(lSize));
-        if (Decrypt(buffer, int(lSize), decrypted.str()))
+        Buffer buffer(static_cast<size_t>(lSize));
+        fread (buffer.Get(), 1, size_t(lSize), file.Get());
+
+        CipherText decrypted(buffer.Get(), static_cast<int>(lSize));
+        if (Envelope_Open(reinterpret_cast<unsigned char *>(buffer.Get()), static_cast<int>(lSize),
+                          encryptedKey, encryptedKeyLen, IV,
+                          reinterpret_cast<unsigned char *>(decrypted.str()), &decrypted.len()))
         {
             plainMessage = decrypted.str();
             //std::cout << plainMessage << std::endl;
@@ -104,58 +112,148 @@ void DecryptFile(const char *inputFile, std::string &plainMessage)
 }
 
 
-bool Encrypt(const char *plainMessage, char *cipherText, int *cipherLen)
+void handleErrors()
 {
-    int bufLen, tmpLen;
+    LOG(ERROR) << "Error in OpenSSL functionality\n";
+}
+
+// https://wiki.openssl.org/index.php/EVP_Asymmetric_Encryption_and_Decryption_of_an_Envelope
+bool Envelope_Seal(const unsigned char *plain_text, int plain_text_len,
+                  unsigned char **encrypted_key, int *encrypted_key_len,
+                  unsigned char *iv,
+                  unsigned char *cipher_text, int *cipher_text_len)
+{
+    int len = 0;
     
-    EVP_CIPHER_CTX *ctx = nullptr;
-    ctx = EVP_CIPHER_CTX_new();
-    if (!ctx)
-        return false;
+    /* Create and initialise the context */
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    if(!ctx)
+        handleErrors();
     
-    EVP_EncryptInit_ex(ctx, MY_CIPHER_MODE, nullptr, key, iv);
+    /* Initialise the envelope seal operation. This operation generates
+     * a key for the provided cipher, and then encrypts that key a number
+     * of times (one for each public key provided in the pub_key array). In
+     * this example the array size is just one. This operation also
+     * generates an IV and places it in iv. */
+    if(EVP_SealInit(ctx, EVP_aes_256_cbc(), encrypted_key, encrypted_key_len, iv, &PUB_KEY, 1) != 1)
+        handleErrors();
     
-    if(!EVP_EncryptUpdate(ctx, (unsigned char *)cipherText, &bufLen, (unsigned char *)plainMessage, strlen(plainMessage)))
-    {
-        return false;
-    }
+    /* Provide the message to be encrypted, and obtain the encrypted output.
+     * EVP_SealUpdate can be called multiple times if necessary
+     */
+    if(EVP_SealUpdate(ctx, cipher_text, cipher_text_len, plain_text, plain_text_len) != 1)
+        handleErrors();
+    *cipher_text_len = len;
     
-    if(!EVP_EncryptFinal_ex(ctx, (unsigned char *)cipherText + bufLen, &tmpLen))
-    {
-        return false;
-    }
+    /* Finalise the encryption. Further cipher_text bytes may be written at
+     * this stage.
+     */
+    if(EVP_SealFinal(ctx, cipher_text + len, &len) != 1)
+        handleErrors();
+    *cipher_text_len += len;
     
-    bufLen += tmpLen;
-    *cipherLen = bufLen;
-    EVP_CIPHER_CTX_cleanup(ctx);
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
     
     return true;
 }
 
-bool Decrypt(const char *cipherText, int cipherLen, char *plainText)
+bool Envelope_Open(unsigned char *cipher_text, int cipher_text_len,
+                  unsigned char *encrypted_key, int encrypted_key_len, unsigned char *iv,
+                  unsigned char *plain_text, int *plain_text_len)
 {
-    int bufLen, tmpLen;
+    int len = 0;
     
-    EVP_CIPHER_CTX *ctx = nullptr;
-    ctx = EVP_CIPHER_CTX_new();
-    if (!ctx)
-        return false;
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
     
-    EVP_DecryptInit_ex(ctx, MY_CIPHER_MODE, nullptr, key, iv);
+    /* Create and initialise the context */
+    if(!ctx)
+        handleErrors();
     
-    if(!EVP_DecryptUpdate(ctx, (unsigned char *)plainText, &bufLen, (const unsigned char *)cipherText, cipherLen))
-    {
-        return false;
-    }
+    /* Initialise the decryption operation. The asymmetric private key is
+     * provided and priv_key, whilst the encrypted session key is held in
+     * encrypted_key */
+    if(EVP_OpenInit(ctx, EVP_aes_256_cbc(), encrypted_key, encrypted_key_len, iv, PRI_KEY) != 1)
+        handleErrors();
     
-    if(!EVP_DecryptFinal_ex(ctx, (unsigned char *)plainText + bufLen, &tmpLen))
-    {
-        return false;
-    }
+    /* Provide the message to be decrypted, and obtain the plaintext output.
+     * EVP_OpenUpdate can be called multiple times if necessary
+     */
+    if(EVP_OpenUpdate(ctx, plain_text, &len, cipher_text, cipher_text_len) != 1)
+        handleErrors();
+    *plain_text_len = len;
     
-    int decryptedLength = bufLen + tmpLen;
-    plainText[decryptedLength] = '\0';
-    EVP_CIPHER_CTX_cleanup(ctx);
+    /* Finalise the decryption. Further plaintext bytes may be written at
+     * this stage.
+     */
+    if(EVP_OpenFinal(ctx, plain_text + len, &len) != 1)
+        handleErrors();
+    *plain_text_len += len;
     
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
     return true;
+}
+
+
+
+File::File(const char *file, const char *mode) :
+        file(fopen(file, mode))
+{
+}
+
+File::~File()
+{
+    fclose(file);
+}
+
+bool File::IsOpen() const
+{
+    return file != nullptr;
+}
+
+FILE *File::Get() const
+{
+    return file;
+}
+
+Buffer::Buffer(size_t sz):
+        ptr(new char[sz])
+{
+}
+
+char *Buffer::Get() const
+{
+    return ptr;
+}
+
+Buffer::~Buffer()
+{
+    delete[] ptr;
+}
+
+CipherText::CipherText(const char *str, int len) :
+        ptr(new char[len]), length(len)
+{
+    std::copy(str, str + len, ptr);
+}
+
+CipherText::~CipherText()
+{
+    delete[] ptr;
+}
+
+char *CipherText::str()
+{
+    return ptr;
+}
+
+int &CipherText::len()
+{
+    return length;
+}
+
+size_t CipherText::size() const
+{
+    return static_cast<size_t>(length);
 }
